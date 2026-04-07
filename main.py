b9392385e21acc27bc246b4753d4d399382f45ba
Laura <laurapapa693@gmail.com>
2026-04-07 13:02:35 -0300

    cadastrar aluno

--- main.py
+++ main.py
@@ -16,3 +16,26 @@ def cadastrar_curso():
             Session.rollback()
             print(f"Ocorreu um erro {erro}")
 cadastrar_curso()
+
+#função criar um aluno com sqlalchemy
+def cadastrar_aluno():
+    with Session() as session:
+        try: 
+            nome = input("Digite o nome do curso para cadastrar o aluno: ").capitalize()
+            curso = session.query(Curso).filter_by(nome=nome).first()
+
+            nome_aluno = input('Digit o nome do aluno: ').capitalize()
+            email_aluno = input("Digite o email: ").capitalize()
+
+            aluno = Aluno(nome=nome_aluno, email=email_aluno, curso=curso)
+
+        except Exception as erro:
+            Session.rollback()
+            print(f"Ocorreu um erro {erro}")
+
+#listar os alunos de um curso
+
+def listar_alunos():
+    lista = Session.queyr().all()
+    print(f"Lista de alunos d")
+
